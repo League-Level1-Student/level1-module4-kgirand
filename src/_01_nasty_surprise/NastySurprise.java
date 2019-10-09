fee4ea5361f162a9a60909b71e457087067c9eb2
League Student <league@iMac-14.attlocal.net>
2019-10-08 19:55:24 -0700

    b, gyfttrgtewe
    sd rgergaf gafsdf sdfgdffg srg d

--- src/_01_nasty_surprise/NastySurprise.java
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -1,31 +1,61 @@
 package _01_nasty_surprise;
 
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class NastySurprise {
-private void showPic(String imageUrl) {
-	try {
-	URL url = new URL(imageUrl);
-	ImageIcon icon = new ImageIcon(url);
-	JLabel imageLabel = new JLabel(icon);
+public class NastySurprise implements ActionListener {
+	JButton button1;
+	JButton button2;
+	public void run() {
+	//URL url = new URL("https://www.digitalspy.com/movies/a28866233/it-chapter-2-director-potential-pennywise-movie/");
+	//ImageIcon icon = new ImageIcon(url);
+	//JLabel imageLabel = new JLabel(icon);
 	JFrame frame = new JFrame();
-	JButton button1 = new JButton("Trick");
-	JButton button2 = new JButton("Treat");
-	frame.add(imageLabel);
+	JPanel panel = new JPanel();
+	button1 = new JButton("Trick");
+	button2 = new JButton("Treat");
+	frame.add(panel);
+	panel.add(button1);
+	panel.add(button2);
+	button1.addActionListener(this);
+	button2.addActionListener(this);
+	//frame.add(imageLabel);
 	frame.setVisible(true);
 	frame.pack();
+	
+}
+	private void showPictureFromTheInternet(String imageUrl) {
+	    try {
+	        URL url = new URL(imageUrl);
+	        Icon icon = new ImageIcon(url);
+	        JLabel imageLabel = new JLabel(icon);
+	        JFrame frame = new JFrame();
+	        frame.add(imageLabel);
+	        frame.setVisible(true);
+	        frame.pack();
+	    } catch (MalformedURLException e) {
+	        e.printStackTrace();
+	    }
 	}
-	catch(MalformedURLException e) {
-		e.printStackTrace();
-}
-}
-public static void main(String[] args) {
-showPic("https://www.digitalspy.com/movies/a28866233/it-chapter-2-director-potential-pennywise-movie/");
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if(e.getSource()==button1) {
+	showPictureFromTheInternet("https://www.nme.com/blogs/the-movies-blog/it-2-film-release-date-casting-trailer-2139755");
+	}
+	else if(e.getSource()==button2) {
+	showPictureFromTheInternet("https://www.goodhousekeeping.com/life/pets/g5053/unique-dog-names/");	
+	}
 }
 }
