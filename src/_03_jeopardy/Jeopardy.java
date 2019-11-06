af3293bacf9007bc6bf87a2c07eadef34e9e4eaf
League Student <league@iMac-5.attlocal.net>
2019-11-05 19:56:29 -0800

    jos fd;ui wtep
    sfa iodhsiahdo df IHO

--- src/_03_jeopardy/Jeopardy.java
+++ src/_03_jeopardy/Jeopardy.java
@@ -34,7 +34,7 @@ import javax.swing.JPanel;
 public class Jeopardy implements ActionListener {
 	private JButton firstButton;
 	private JButton secondButton;
-	private JButton thirdButton, fourthButton;
+	private JButton thirdButton, fourthButton, fifthButton;
 	private JPanel quizPanel;
 	private int score = 0;
 	private JLabel scoreBox = new JLabel("0");
@@ -48,21 +48,17 @@ public class Jeopardy implements ActionListener {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		quizPanel = new JPanel();
 		frame.setLayout(new BorderLayout());
-
 		// 1. Make the frame show up
-		frame.pack();
 		frame.setVisible(true);
 		// 2. Give your frame a title
 		frame.setTitle("Jeapardy");
 		// 3. Create a JPanel variable to hold the header using the createHeader method
-		JPanel panel = new JPanel();
-		panel = createHeader("Science and Technology");
 		// 4. Add the header component to the quizPanel
 		quizPanel.add(createHeader("Science and Technology"));
 		// 5. Add the quizPanel to the frame
 		frame.add(quizPanel);
 		// 6. Use the createButton method to set the value of firstButton
-		createButton("100");
+		firstButton = createButton("100");
 		// 7. Add the firstButton to the quizPanel
 		quizPanel.add(firstButton);
 		// 8. Write the code to complete the createButton() method below. Check that your
@@ -70,38 +66,48 @@ public class Jeopardy implements ActionListener {
 
 		// 9. Use the secondButton variable to hold a button using the createButton
 		// method
-
+		secondButton = createButton("200");
 		// 10. Add the secondButton to the quizPanel
-
+		quizPanel.add(secondButton);
 		// 11. Add action listeners to the buttons (2 lines of code)
-
+		firstButton.addActionListener(this);
+		secondButton.addActionListener(this);
 		// 12. Write the code to complete the actionPerformed() method below
 
 		// 13. Add buttons so that you have $200, $400, $600, $800 and $1000 questions
-		
-		 /*
+		thirdButton = createButton("300");
+		quizPanel.add(thirdButton);
+		fourthButton = createButton("400");
+		quizPanel.add(fourthButton);
+		fifthButton = createButton("500");
+		quizPanel.add(fifthButton);
+		thirdButton.addActionListener(this);
+		fourthButton.addActionListener(this); 
+		fifthButton.addActionListener(this);
+		/*
 		 * [optional] Use the showImage or playSound methods when the user answers a
 		 * question
 		 */		
 		
-		frame.pack();
+	
 		quizPanel.setLayout(new GridLayout(buttonCount + 1, 3));
 		frame.add(makeScorePanel(), BorderLayout.NORTH);
 		frame.setSize(Toolkit.getDefaultToolkit().getScreenSize().height,
 				Toolkit.getDefaultToolkit().getScreenSize().width);
+		frame.pack();
 	}
 
 
 	private JButton createButton(String dollarAmount) {
 		
 		// Create a new JButton
-		JButton button100 = new JButton("100");
+		JButton button100 = new JButton(dollarAmount);
 		// Set the text of the button to the dollarAmount
 		// Increment the buttonCount (this should make the layout vertical)
 		buttonCount ++;
 		// Return your new button instead of the temporary button
 		
-		return new JButton("temporary button");
+		return button100;
 	}
 
 	public void actionPerformed(ActionEvent e) {
@@ -111,17 +117,33 @@ public class Jeopardy implements ActionListener {
 
 		JButton buttonPressed = (JButton) e.getSource();
 		// If the buttonPressed was the firstButton
-
+		if(buttonPressed.getText() == ""){
+			return;
+		}
+		if(buttonPressed == firstButton) {
+			askQuestion("Who is the founder of apple?", "Steve Jobs", 100);
+		}
 			// Call the askQuestion() method
  
 		// Complete the code in the askQuestion() method. When you play the game, the score should change.
 
 		// If the buttonPressed was the secondButton
-
+		if(buttonPressed == secondButton) {
+			askQuestion("What is the main output of photosynthesis?", "oxygen", 200);
+		}
+		if(buttonPressed == thirdButton) {
+			askQuestion("What are the main inputs for Cellular Respiration? (lower case and gas first)", "oxygen and glucose", 300);	
+		}
+		if(buttonPressed == fourthButton) {
+			askQuestion("Who is the second founder/partner founder of apple?", "Steve Wozniak", 400);
+		}
+		if(buttonPressed == fifthButton) {
+			askQuestion("When was Apple Inc. founded? (Lower case)","april 1, 1976", 500);
+		}
 			// Call the askQuestion() method with a harder question
-
+		
 		// Clear the text on the button that was pressed (set the button text to nothing)
-
+		buttonPressed.setText("");
 	}
 
 	private void askQuestion(String question, String correctAnswer, int prizeMoney) {
@@ -129,24 +151,26 @@ public class Jeopardy implements ActionListener {
 		// Use the playJeopardyTheme() method to play music while the use thinks of an answer
 		
 		// Remove this temporary message and replace it with a pop-up that asks the user the question
-		JOptionPane.showMessageDialog(null, "this is where the question will be asked");
+		String input = JOptionPane.showInputDialog(null, question);
 		
 		// Stop the theme music when they have entered their response. Hint: use the sound variable 
 		
 		// If the answer is correct
-
+		if(input.equals(correctAnswer)) {
 			// Increase the score by the prizeMoney
-
+			score += prizeMoney;
 			// Pop up a message to tell the user they were correct
-
+			JOptionPane.showMessageDialog(null, "Congrats. You got it right");
+		}
 		// Otherwise
-
+		else {
 			// Decrement the score by the prizeMoney
-
+			score -= prizeMoney;
 			// Pop up a message to tell the user they were wrong and give them the correct answer
-
+			JOptionPane.showMessageDialog(null, "Congrats. You got it wrong. The correct answer is " + correctAnswer);
 		// Call the updateScore() method
-
+		}
+		updateScore();
 	}
 
 	public void playJeopardyTheme() {
