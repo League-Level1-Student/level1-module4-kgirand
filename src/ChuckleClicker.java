801f0feebac75fc348aedccb3d1bcc573d066af7
League Student <league@iMac-5.attlocal.net>
2019-11-18 19:55:35 -0800

    ghjtyhjdty
    fgjhft hdtyhdr ty

---
+++ src/ChuckleClicker.java
@@ -0,0 +1,41 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class ChuckleClicker implements ActionListener {
+JButton button1;
+JButton button2;
+public static void main(String[] args) {
+	ChuckleClicker CC = new ChuckleClicker();
+	CC.makeButtons();	
+}
+public void makeButtons () {
+JFrame frame = new JFrame();
+frame.setVisible(true);
+JPanel panel = new JPanel();
+frame.add(panel);
+button1 = new JButton("joke");
+button2 = new JButton("punchline");
+panel.add(button1);
+panel.add(button2);
+
+button1.addActionListener(this);
+button2.addActionListener(this);
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	JButton buttonPressed = (JButton) e.getSource();
+	if(buttonPressed == button1) {
+		JOptionPane.showMessageDialog(null, "What did the fish say when he ran into the wall?");
+	}
+	if(buttonPressed == button2) {
+		JOptionPane.showMessageDialog(null, "Dam");
+	}
+}
+
+}
---
+++ src/TypingTutor.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TypingTutor {
+char currentLetter;
+public static void main(String[] args) {
+JFrame frame = new JFrame("Type or Die");
+frame.setVisible(true);
+JLabel label = new JLabel();
+JPanel panel = new JPanel();
+label.setText("" + generateRandomLetter());
+label.setFont(label.getFont().deriveFont(28));
+label.setHorizontalAlignment(label.CENTER);
+}
+
+static char generateRandomLetter() {
+	Random r = new Random();
+	return(char)(r.nextInt(26)+ 'a');
+}
+}
